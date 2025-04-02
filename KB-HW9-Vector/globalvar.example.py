ddf570216582cfe2799ca6c68afb74a76cf9291d
ivanxma <ivanxma@yahoo.com>
2025-04-02 22:58:18 +0100

    add global variables example

--- KB-HW9-Vector/globalvar.example.py
+++ KB-HW9-Vector/globalvar.example.py
@@ -6,6 +6,10 @@ CONFIG_PROFILE = "DEFAULT"
 # Service endpoint
 endpoint = "https://inference.generativeai.us-chicago-1.oci.oraclecloud.com"
 
+llm = "cohere.command-r-plus-08-2024"
+emb_modelid = "multilingual-e5-small"
+citations = 15
+
 myconfig = {
     "user":"<user>",
     "password":"<password>",
