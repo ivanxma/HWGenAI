546a2d823b18a7f87b77f167154f3a3535343181
ivanxma <ivanxma@yahoo.com>
2025-04-02 23:00:10 +0100

    add demo description

--- KB-HW9-Vector/main.py
+++ KB-HW9-Vector/main.py
@@ -2,9 +2,10 @@ import streamlit as st
 def main():
     st.header("HeatWave Vector Store with OCI GenAI")   
     st.divider()
-    st.write("This is a DEMO using HeatWave 9.0 on OCI")
+    st.write("This is a DEMO using HeatWave 9.2.2 on OCI")
     st.write("The \"Create KB from URL\" page is to fetch URL content and creats embedding vector into MySQL Vector Store")
     st.write("The \"Search KB \" page is to use the GenAI to generate answer from the KB vector store in MySQL")
+    st.write("The \"Summarize \" page is to fetch URL content and summarize")
     st.divider()
 
             
